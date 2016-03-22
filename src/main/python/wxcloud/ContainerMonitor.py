fcd9f9b60aa08a61fa9b58184beb5e0dd5e19689
zhuanghua <30758919@qq.com>
2016-03-22 10:56:42 +0800

    blinker

--- src/main/python/wxcloud/ContainerMonitor.py
+++ src/main/python/wxcloud/ContainerMonitor.py
@@ -5,14 +5,16 @@ Created on Mar 21, 2016
 '''
 
 from ahab import Ahab
+import containersignal
 
 def f(event, data):
 #    print 'event=',event
     print("status",event['status'],'from',event['from'])
     print 'data=',data
 #    pass        # Handle the Docker event (and extended info, as available)
-      
+    containersignal.containerdestroy.send(event)
 
+def startAhab():
 
 
 if __name__ == '__main__':
